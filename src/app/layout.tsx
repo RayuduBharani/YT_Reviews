85705e6764165397f3ff3f0aa2acf6b691e81dfc
Rayudu Bharani <rayudubharani7288@gmail.com>
2025-01-26 16:48:02 +0530

    Err solved 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const poppins = Poppins({
   weight:["100" , "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
+  preload: true,
 });
 
 export const metadata: Metadata = {
